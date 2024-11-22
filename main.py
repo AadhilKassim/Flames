81cb22a53e45202a3dcf6fc76d4effc607ba0561
Aadhil Kassim <aadhikassim@gmail.com>
2024-11-22 22:31:08 +0530

    Simplified the whitespace removal and Added a loop to remove the common letters

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 name1 = list(input("Enter your name: ").lower())
 name2 = list(input("Enter your crush's name: ").lower())
 
-for element in name1:
-    if element == " ":
-        name1.pop(name1.index(" "))
-for element in name2:
-    if element == " ":
-        name2.pop(name2.index(" "))
+name1 = [char for char in name1 if char != " "]
+name2 = [char for char in name2 if char != " "]
 
 common_letters = set(name1).intersection(set(name2))
 print(common_letters)
+
+for char in common_letters:
+    # Remove all occurrences of the character in name1
+    name1 = [c for c in name1 if c != char]
+    # Remove all occurrences of the character in name2
+    name2 = [c for c in name2 if c != char]
 
 
 
